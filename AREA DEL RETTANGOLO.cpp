09f9943d38e617242f53271bdea1ad75f1f009c2
ANTONIO <157252253+RYZ307@users.noreply.github.com>
2024-01-21 14:58:31 +0100

    Add files via upload

---
+++ AREA DEL RETTANGOLO.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+ int base;
+ int altezza;
+
+ cout<<"inserisci base:";
+ cin>>base;
+
+
+ cout<<"inserisci altezza:";
+ cin>>altezza;
+
+ int area = base*altezza;
+ cout<<"l'area del rettangolo e':"<<area;
+
+
+}
